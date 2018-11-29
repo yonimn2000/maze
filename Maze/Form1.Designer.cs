ecfe89998919fbd86af97a754d26f2868d99af1f
Yonatan <yonimn2000@gmail.com>
2018-11-28 20:37:59 -0500

    v0.1 Works

--- Maze/Form1.Designer.cs
+++ Maze/Form1.Designer.cs
@@ -29,12 +29,212 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.MazeBox = new System.Windows.Forms.PictureBox();
+            this.FrameTimer = new System.Windows.Forms.Timer(this.components);
+            this.FPS_NUD = new System.Windows.Forms.NumericUpDown();
+            this.GenerateMazeBTN = new System.Windows.Forms.Button();
+            this.SaveBTN = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.WidthNUD = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.HeightNUD = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SlowlyCB = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MazeBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FPS_NUD)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WidthNUD)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HeightNUD)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MazeBox
+            // 
+            this.MazeBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MazeBox.BackColor = System.Drawing.SystemColors.Control;
+            this.MazeBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MazeBox.Location = new System.Drawing.Point(129, 0);
+            this.MazeBox.Name = "MazeBox";
+            this.MazeBox.Size = new System.Drawing.Size(855, 461);
+            this.MazeBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.MazeBox.TabIndex = 0;
+            this.MazeBox.TabStop = false;
+            // 
+            // FrameTimer
+            // 
+            this.FrameTimer.Interval = 1000;
+            this.FrameTimer.Tick += new System.EventHandler(this.FrameTimer_Tick);
+            // 
+            // FPS_NUD
+            // 
+            this.FPS_NUD.Location = new System.Drawing.Point(49, 33);
+            this.FPS_NUD.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.FPS_NUD.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.FPS_NUD.Name = "FPS_NUD";
+            this.FPS_NUD.Size = new System.Drawing.Size(74, 20);
+            this.FPS_NUD.TabIndex = 1;
+            this.FPS_NUD.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.FPS_NUD.ValueChanged += new System.EventHandler(this.FPS_NUD_ValueChanged);
+            // 
+            // GenerateMazeBTN
+            // 
+            this.GenerateMazeBTN.Location = new System.Drawing.Point(64, 7);
+            this.GenerateMazeBTN.Name = "GenerateMazeBTN";
+            this.GenerateMazeBTN.Size = new System.Drawing.Size(59, 23);
+            this.GenerateMazeBTN.TabIndex = 2;
+            this.GenerateMazeBTN.Text = "Generate";
+            this.GenerateMazeBTN.UseVisualStyleBackColor = true;
+            this.GenerateMazeBTN.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // SaveBTN
+            // 
+            this.SaveBTN.Location = new System.Drawing.Point(8, 111);
+            this.SaveBTN.Name = "SaveBTN";
+            this.SaveBTN.Size = new System.Drawing.Size(115, 23);
+            this.SaveBTN.TabIndex = 3;
+            this.SaveBTN.Text = "Save";
+            this.SaveBTN.UseVisualStyleBackColor = true;
+            this.SaveBTN.Click += new System.EventHandler(this.SaveBTN_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "FPS";
+            // 
+            // WidthNUD
+            // 
+            this.WidthNUD.Location = new System.Drawing.Point(49, 59);
+            this.WidthNUD.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.WidthNUD.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.WidthNUD.Name = "WidthNUD";
+            this.WidthNUD.Size = new System.Drawing.Size(74, 20);
+            this.WidthNUD.TabIndex = 1;
+            this.WidthNUD.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.WidthNUD.ValueChanged += new System.EventHandler(this.WidthNUD_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Width";
+            // 
+            // HeightNUD
+            // 
+            this.HeightNUD.Location = new System.Drawing.Point(49, 85);
+            this.HeightNUD.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.HeightNUD.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.HeightNUD.Name = "HeightNUD";
+            this.HeightNUD.Size = new System.Drawing.Size(74, 20);
+            this.HeightNUD.TabIndex = 1;
+            this.HeightNUD.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.HeightNUD.ValueChanged += new System.EventHandler(this.HeightNUD_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Height";
+            // 
+            // SlowlyCB
+            // 
+            this.SlowlyCB.AutoSize = true;
+            this.SlowlyCB.Location = new System.Drawing.Point(12, 10);
+            this.SlowlyCB.Name = "SlowlyCB";
+            this.SlowlyCB.Size = new System.Drawing.Size(56, 17);
+            this.SlowlyCB.TabIndex = 5;
+            this.SlowlyCB.Text = "Slowly";
+            this.SlowlyCB.UseVisualStyleBackColor = true;
+            this.SlowlyCB.CheckedChanged += new System.EventHandler(this.SlowlyCB_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(984, 461);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SaveBTN);
+            this.Controls.Add(this.GenerateMazeBTN);
+            this.Controls.Add(this.HeightNUD);
+            this.Controls.Add(this.WidthNUD);
+            this.Controls.Add(this.FPS_NUD);
+            this.Controls.Add(this.MazeBox);
+            this.Controls.Add(this.SlowlyCB);
+            this.MinimumSize = new System.Drawing.Size(200, 200);
+            this.Name = "Form1";
+            this.Text = "Maze Generator";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.MazeBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FPS_NUD)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.WidthNUD)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.HeightNUD)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox MazeBox;
+        private System.Windows.Forms.Timer FrameTimer;
+        private System.Windows.Forms.NumericUpDown FPS_NUD;
+        private System.Windows.Forms.Button GenerateMazeBTN;
+        private System.Windows.Forms.Button SaveBTN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown WidthNUD;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown HeightNUD;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox SlowlyCB;
     }
 }
 
--- Maze/Form1.cs
+++ Maze/Form1.cs
@@ -1,20 +1,246 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Maze
 {
     public partial class Form1 : Form
     {
+        Maze maze;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            FPS_NUD_ValueChanged(sender, e);
+        }
+
+        private void FPS_NUD_ValueChanged(object sender, EventArgs e)
+        {
+            FrameTimer.Interval = (int)(1000 / FPS_NUD.Value);
+        }
+
+        private void FrameTimer_Tick(object sender, EventArgs e)
+        {
+            if (!maze.IsDone)
+                maze.Step();
+            else
+                FrameTimer.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (MazeBox.Height >= HeightNUD.Value && MazeBox.Width >= WidthNUD.Value)
+            {
+                maze = new Maze((int)HeightNUD.Value, (int)WidthNUD.Value, 1, MazeBox.CreateGraphics());
+            }
+            else
+            {
+                maze = new Maze((int)HeightNUD.Value, (int)WidthNUD.Value, 1);
+            }
+            if (SlowlyCB.Checked)
+                FrameTimer.Enabled = true;
+            else
+                maze.DrawAll();
+        }
+
+        private void SaveBTN_Click(object sender, EventArgs e)
+        {
+            maze.Save("maze.bmp");
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            MazeBox.Width = Width - 145;
+        }
+
+        private void SlowlyCB_CheckedChanged(object sender, EventArgs e)
+        {
+            if (FrameTimer.Enabled)
+                if (!maze.IsDone)
+                {
+                    FrameTimer.Enabled = false;
+                    maze.DrawAll();
+                }
+        }
+
+        private void WidthNUD_ValueChanged(object sender, EventArgs e)
+        {
+            MazeBox.Width = (int)WidthNUD.Value;
+        }
+
+        private void HeightNUD_ValueChanged(object sender, EventArgs e)
+        {
+            MazeBox.Height = (int)HeightNUD.Value;
+        }
+    }
+
+    class Maze
+    {
+        Bitmap Bitmap;
+        Graphics Graphics;
+        Cell[] Cells;
+        int Rows;
+        int Columns;
+        int PixelWidth;
+        public bool IsDone;
+        public Maze(int hieght, int width, int pixelWidth, Graphics graphics = null)
+        {
+            PixelWidth = pixelWidth;
+            IsDone = false;
+            Rows = hieght / pixelWidth;
+            Columns = width / pixelWidth; ;
+            if (Columns % 2 == 0)
+                Columns--;
+            if (Rows % 2 == 0)
+                Rows--;
+            Cells = new Cell[Rows * Columns];
+            Bitmap = new Bitmap(Columns, Rows);
+            Graphics = graphics != null ? graphics : Graphics.FromImage(Bitmap);
+            for (int y = 0; y < Rows; y++)
+                for (int x = 0; x < Columns; x++)
+                {
+                    Cells[Index(x, y)] = new Cell(x, y);
+                    if (y == 0 || y == Rows - 1 || x == 0 || x == Columns - 1 || x % 2 == 0 || y % 2 == 0)
+                        Cells[Index(x, y)].IsWall = true;
+                }
+        }
+
+        private int Index(int x, int y)
+        {
+            if (y < 0 || x < 0 || x > Columns - 1 || y > Rows - 1)
+                return -1;
+            return x + y * Columns;
+        }
+
+        public void Draw()
+        {
+            for (int y = 0; y < Rows; y++)
+                for (int x = 0; x < Columns; x++)
+                    Graphics.FillRectangle(new SolidBrush(Cells[Index(x, y)].IsWall ? Color.Black : Color.White), x * PixelWidth, y * PixelWidth, PixelWidth, PixelWidth);
+        }
+
+        public void DrawAll()
+        {
+            while (!IsDone) //Step 2
+                Step();
+        }
+
+        Cell currentCell;
+        public int stepsCount = 0;
+        Stack<Cell> cellsStack = new Stack<Cell>();
+        Random Random = new Random();
+        public void Step()
+        {
+            if (stepsCount == 0)
+            {
+                Graphics.FillRectangle(new SolidBrush(Color.Black), 0, 0, Columns * PixelWidth, Rows * PixelWidth);
+                currentCell = Cells[Index(1, 1)]; //Step 1
+            }
+            currentCell.IsVisited = true; //Step 2.1.4
+            currentCell.Draw(Color.White, PixelWidth, Graphics);
+            List<Cell> neighboringCells = GetUnvisitedNeighboringCells(currentCell);
+            if (neighboringCells.Count > 0) //Step 2.1
+            {
+                Cell chosenCell = neighboringCells[Random.Next(neighboringCells.Count)]; //Step 2.1.1
+                cellsStack.Push(currentCell); //Step 2.1.2
+                RemoveWall(currentCell, chosenCell); //Step 2.1.3
+                currentCell = chosenCell; //Step 2.1.4
+            }
+            else if (cellsStack.Count > 0) //Step 2.2
+                currentCell = cellsStack.Pop(); //Step 2.2.1 and 2.2.2
+            else
+                IsDone = true;
+            stepsCount++;
+            if (IsDone)
+            {
+                Cells[Index(1, 0)].IsWall = false; //Start cell
+                Cells[Index(Columns - 2, Rows - 1)].IsWall = false; //End cell
+            }
+        }
+
+        private void RemoveWall(Cell currentCell, Cell chosenCell)
+        {
+            int index = 0;
+            if (currentCell.X - chosenCell.X == -2)
+                index = Index(currentCell.X + 1, currentCell.Y);
+            else if (currentCell.X - chosenCell.X == 2)
+                index = Index(currentCell.X - 1, currentCell.Y);
+            else if (currentCell.Y - chosenCell.Y == -2)
+                index = Index(currentCell.X, currentCell.Y + 1);
+            else if (currentCell.Y - chosenCell.Y == 2)
+                index = Index(currentCell.X, currentCell.Y - 1);
+            Cells[index].IsWall = false;
+            Cells[index].Draw(Color.White, PixelWidth, Graphics);
+            //Graphics.FillRectangle(new SolidBrush(Color.White), Cells[index].X * PixelWidth, Cells[index].Y * PixelWidth, PixelWidth, PixelWidth);
+        }
+
+        private List<Cell> GetUnvisitedNeighboringCells(Cell currentCell)
+        {
+            List<Cell> neighboringCells = new List<Cell>();
+            AddNeighboringCells(neighboringCells, Index(currentCell.X + 2, currentCell.Y));
+            AddNeighboringCells(neighboringCells, Index(currentCell.X - 2, currentCell.Y));
+            AddNeighboringCells(neighboringCells, Index(currentCell.X, currentCell.Y + 2));
+            AddNeighboringCells(neighboringCells, Index(currentCell.X, currentCell.Y - 2));
+            return neighboringCells;
+        }
+
+        private void AddNeighboringCells(List<Cell> neighboringCells, int index)
+        {
+            if (index > 0)
+            {
+                Cell cell = Cells[index];
+                if (!cell.IsVisited)
+                    neighboringCells.Add(cell);
+            }
+        }
+
+        public void Save(string fileName)
+        {
+            for (int y = 0; y < Rows; y++)
+                for (int x = 0; x < Columns; x++)
+                    Bitmap.SetPixel(x, y, Cells[Index(x, y)].IsWall ? Color.Black : Color.White);
+            Bitmap.Save(fileName);
+            MessageBox.Show("Saved to " + Application.ExecutablePath);
+        }
+    }
+
+    class Cell
+    {
+        public int X;
+        public int Y;
+        public bool IsVisited;
+        public bool IsWall;
+
+        public Cell(int x, int y)
+        {
+            X = x;
+            Y = y;
+            IsVisited = false;
+            IsWall = false;
+        }
+
+        public void Draw(Color color, int width, Graphics graphics)
+        {
+            graphics.FillRectangle(new SolidBrush(Color.White), X * width, Y * width, width, width);
+        }
+
+        public override string ToString()
+        {
+            return $"({X}, {Y}), {(IsVisited ? "" : "Not ")}Visited, {(IsWall ? "" : "Not ")}Wall";
+        }
     }
 }
+/*  1 Make the initial cell the current cell and mark it as visited
+    2 While there are unvisited cells
+    2.1 If the current cell has any neighbors which have not been visited
+    2.1.1 Choose randomly one of the unvisited neighbours
+    2.1.2 Push the current cell to the stack
+    2.1.3 Remove the wall between the current cell and the chosen cell
+    2.1.4 Make the chosen cell the current cell and mark it as visited
+    2.2 Else if stack is not empty
+    2.2.1 Pop a cell from the stack
+    2.2.2 Make it the current cell */
