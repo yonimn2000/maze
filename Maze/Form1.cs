e734885fe8849529ec40838bcc8d20124c0a88d5
Yonatan <yonimn2000@gmail.com>
2018-12-01 10:29:16 -0500

    v0.3 Code cleanup

--- Maze/Form1.cs
+++ Maze/Form1.cs
@@ -1,13 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
 using System.Windows.Forms;
 
 namespace Maze
 {
     public partial class Form1 : Form
     {
-        Maze maze;
+        Maze.Maker maze;
         public Form1()
         {
             InitializeComponent();
@@ -26,7 +24,7 @@ namespace Maze
         private void FrameTimer_Tick(object sender, EventArgs e)
         {
             if (!maze.IsDone)
-                maze.Step();
+                maze.MakeStep();
             else
                 FrameTimer.Enabled = false;
         }
@@ -34,16 +32,16 @@ namespace Maze
         private void button1_Click(object sender, EventArgs e)
         {
             int width = Math.Min((Width - 145) / (int)WidthNUD.Value, (Height - 40) / (int)HeightNUD.Value);
-            maze = new Maze((int)HeightNUD.Value* width, (int)WidthNUD.Value* width, width, MazeBox.CreateGraphics());
+            maze = new Maze.Maker((int)WidthNUD.Value, (int)HeightNUD.Value);
             if (SlowlyCB.Checked)
                 FrameTimer.Enabled = true;
             else
-                maze.DrawAll();
+                maze.Make();
         }
 
         private void SaveBTN_Click(object sender, EventArgs e)
         {
-            maze.Save("maze.bmp");
+            maze.SaveAsImage("maze.bmp");
         }
 
         private void Form1_SizeChanged(object sender, EventArgs e)
@@ -63,173 +61,8 @@ namespace Maze
                 if (!maze.IsDone)
                 {
                     FrameTimer.Enabled = false;
-                    maze.DrawAll();
+                    maze.Make();
                 }
-        }
-    }
-
-    class Maze
-    {
-        Bitmap Bitmap;
-        Graphics Graphics;
-        Cell[] Cells;
-        int Rows;
-        int Columns;
-        int PixelWidth;
-        public bool IsDone;
-        public Maze(int hieght, int width, int pixelWidth, Graphics graphics = null)
-        {
-            PixelWidth = pixelWidth;
-            IsDone = false;
-            Rows = hieght / pixelWidth;
-            Columns = width / pixelWidth;
-            if (Columns % 2 == 0)
-                Columns--;
-            if (Rows % 2 == 0)
-                Rows--;
-            Cells = new Cell[Rows * Columns];
-            Bitmap = new Bitmap(Columns, Rows);
-            Graphics = graphics != null ? graphics : Graphics.FromImage(Bitmap);
-            for (int y = 0; y < Rows; y++)
-                for (int x = 0; x < Columns; x++)
-                {
-                    Cells[Index(x, y)] = new Cell(x, y);
-                    if (y == 0 || y == Rows - 1 || x == 0 || x == Columns - 1 || x % 2 == 0 || y % 2 == 0)
-                        Cells[Index(x, y)].IsWall = true;
-                }
-            Cells[Index(1, 0)].IsWall = false; //Start cell
-            Cells[Index(Columns - 2, Rows - 1)].IsWall = false; //End cell
-        }
-
-        private int Index(int x, int y)
-        {
-            if (y < 0 || x < 0 || x > Columns - 1 || y > Rows - 1)
-                return -1;
-            return x + y * Columns;
-        }
-
-        public void Draw()
-        {
-            Graphics.Clear(Color.White);
-            for (int y = 0; y < Rows; y++)
-                for (int x = 0; x < Columns; x++)
-                    Graphics.FillRectangle(new SolidBrush(Cells[Index(x, y)].IsWall ? Color.Black : Color.White), x * PixelWidth, y * PixelWidth, PixelWidth, PixelWidth);
-        }
-
-        public void DrawAll()
-        {
-            Graphics.Clear(Color.White);
-            while (!IsDone) //Step 2
-                Step();
-        }
-
-        Cell currentCell;
-        public int stepsCount = 0;
-        Stack<Cell> cellsStack = new Stack<Cell>();
-        Random Random = new Random();
-        public void Step()
-        {
-            if (stepsCount == 0)
-            {
-                Graphics.FillRectangle(new SolidBrush(Color.Black), 0, 0, Columns * PixelWidth, Rows * PixelWidth);
-                Cells[Index(1, 0)].Draw(Color.White, PixelWidth, Graphics);
-                Cells[Index(Columns - 2, Rows - 1)].Draw(Color.White, PixelWidth, Graphics);
-                currentCell = Cells[Index(1, 1)]; //Step 1
-            }
-            currentCell.IsVisited = true; //Step 2.1.4
-            currentCell.Draw(Color.White, PixelWidth, Graphics);
-            List<Cell> neighboringCells = GetUnvisitedNeighboringCells(currentCell);
-            if (neighboringCells.Count > 0) //Step 2.1
-            {
-                Cell chosenCell = neighboringCells[Random.Next(neighboringCells.Count)]; //Step 2.1.1
-                cellsStack.Push(currentCell); //Step 2.1.2
-                RemoveWall(currentCell, chosenCell); //Step 2.1.3
-                currentCell = chosenCell; //Step 2.1.4
-            }
-            else if (cellsStack.Count > 0) //Step 2.2
-                currentCell = cellsStack.Pop(); //Step 2.2.1 and 2.2.2
-            else
-                IsDone = true;
-            stepsCount++;
-        }
-
-        private void RemoveWall(Cell currentCell, Cell chosenCell)
-        {
-            int index = 0;
-            if (currentCell.X - chosenCell.X == -2)
-                index = Index(currentCell.X + 1, currentCell.Y);
-            else if (currentCell.X - chosenCell.X == 2)
-                index = Index(currentCell.X - 1, currentCell.Y);
-            else if (currentCell.Y - chosenCell.Y == -2)
-                index = Index(currentCell.X, currentCell.Y + 1);
-            else if (currentCell.Y - chosenCell.Y == 2)
-                index = Index(currentCell.X, currentCell.Y - 1);
-            Cells[index].IsWall = false;
-            Cells[index].Draw(Color.White, PixelWidth, Graphics);
-        }
-
-        private List<Cell> GetUnvisitedNeighboringCells(Cell currentCell)
-        {
-            List<Cell> neighboringCells = new List<Cell>();
-            AddNeighboringCells(neighboringCells, Index(currentCell.X + 2, currentCell.Y));
-            AddNeighboringCells(neighboringCells, Index(currentCell.X - 2, currentCell.Y));
-            AddNeighboringCells(neighboringCells, Index(currentCell.X, currentCell.Y + 2));
-            AddNeighboringCells(neighboringCells, Index(currentCell.X, currentCell.Y - 2));
-            return neighboringCells;
-        }
-
-        private void AddNeighboringCells(List<Cell> neighboringCells, int index)
-        {
-            if (index > 0)
-            {
-                Cell cell = Cells[index];
-                if (!cell.IsVisited)
-                    neighboringCells.Add(cell);
-            }
-        }
-
-        public void Save(string fileName)
-        {
-            for (int y = 0; y < Rows; y++)
-                for (int x = 0; x < Columns; x++)
-                    Bitmap.SetPixel(x, y, Cells[Index(x, y)].IsWall ? Color.Black : Color.White);
-            Bitmap.Save(fileName);
-            MessageBox.Show("Saved to " + Application.ExecutablePath);
-        }
-        class Cell
-        {
-            public int X;
-            public int Y;
-            public bool IsVisited;
-            public bool IsWall;
-
-            public Cell(int x, int y)
-            {
-                X = x;
-                Y = y;
-                IsVisited = false;
-                IsWall = false;
-            }
-
-            public void Draw(Color color, int width, Graphics graphics)
-            {
-                graphics.FillRectangle(new SolidBrush(Color.White), X * width, Y * width, width, width);
-            }
-
-            public override string ToString()
-            {
-                return $"({X}, {Y}), {(IsVisited ? "" : "Not ")}Visited, {(IsWall ? "" : "Not ")}Wall";
-            }
         }
     }
 }
-/*  1 Make the initial cell the current cell and mark it as visited
-    2 While there are unvisited cells
-    2.1 If the current cell has any neighbors which have not been visited
-    2.1.1 Choose randomly one of the unvisited neighbours
-    2.1.2 Push the current cell to the stack
-    2.1.3 Remove the wall between the current cell and the chosen cell
-    2.1.4 Make the chosen cell the current cell and mark it as visited
-    2.2 Else if stack is not empty
-    2.2.1 Pop a cell from the stack
-    2.2.2 Make it the current cell */
---
+++ Maze/Maze.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Maze
+{
+    class Maze
+    {
+        public Cell[] Cells { get; private set; }
+        public int Rows { get; private set; }
+        public int Columns { get; private set; }
+
+        public Maze(int columns, int rows)
+        {
+            Rows = rows;
+            Columns = columns;
+            if (Columns % 2 == 0)
+                Columns--;
+            if (Rows % 2 == 0)
+                Rows--;
+            Cells = new Cell[Rows * Columns];
+            for (int y = 0; y < Rows; y++)
+                for (int x = 0; x < Columns; x++)
+                {
+                    Cells[CellCoordinate(x, y)] = new Cell(x, y);
+                    if (y == 0 || y == Rows - 1 || x == 0 || x == Columns - 1 || x % 2 == 0 || y % 2 == 0)
+                        Cells[CellCoordinate(x, y)].IsWall = true; //Set walls around
+                }
+            Cells[CellCoordinate(1, 0)].IsWall = false; //Start cell
+            Cells[CellCoordinate(Columns - 2, Rows - 1)].IsWall = false; //End cell
+        }
+
+        public int CellCoordinate(int x, int y)
+        {
+            if (y < 0 || x < 0 || x > Columns - 1 || y > Rows - 1)
+                return -1;
+            return x + y * Columns;
+        }
+
+        public List<Cell> GetUnvisitedNeighboringCellsList(Cell currentCell)
+        {
+            List<Cell> neighboringCells = new List<Cell>();
+            AddNeighboringCellsToList(neighboringCells, CellCoordinate(currentCell.X + 2, currentCell.Y));
+            AddNeighboringCellsToList(neighboringCells, CellCoordinate(currentCell.X - 2, currentCell.Y));
+            AddNeighboringCellsToList(neighboringCells, CellCoordinate(currentCell.X, currentCell.Y + 2));
+            AddNeighboringCellsToList(neighboringCells, CellCoordinate(currentCell.X, currentCell.Y - 2));
+            return neighboringCells;
+        }
+
+        private void AddNeighboringCellsToList(List<Cell> neighboringCells, int index)
+        {
+            if (index > 0)
+            {
+                Cell cell = Cells[index];
+                if (!cell.IsVisited)
+                    neighboringCells.Add(cell);
+            }
+        }
+
+        public void SaveAsImage(string fileName)
+        {
+            Bitmap bitmap = new Bitmap(Columns, Rows);
+            for (int y = 0; y < Rows; y++)
+                for (int x = 0; x < Columns; x++)
+                    bitmap.SetPixel(x, y, Cells[CellCoordinate(x, y)].IsWall ? Color.Black : Color.White);
+            bitmap.Clone(new Rectangle(0,0,bitmap.Width,bitmap.Height), PixelFormat.Format1bppIndexed).Save(fileName);
+            System.Diagnostics.Process.Start(fileName);
+        }
+
+        public class Cell
+        {
+            public int X;
+            public int Y;
+            public bool IsVisited;
+            public bool IsWall;
+
+            public Cell(int x, int y)
+            {
+                X = x;
+                Y = y;
+                IsVisited = false;
+                IsWall = false;
+            }
+
+            public override string ToString()
+            {
+                return $"({X}, {Y}), {(IsVisited ? "" : "Not ")}Visited, {(IsWall ? "" : "Not ")}Wall";
+            }
+        }
+
+        /*  1 Make the initial cell the current cell and mark it as visited
+            2 While there are unvisited cells
+            2.1 If the current cell has any neighbors which have not been visited
+            2.1.1 Choose randomly one of the unvisited neighbours
+            2.1.2 Push the current cell to the stack
+            2.1.3 Remove the wall between the current cell and the chosen cell
+            2.1.4 Make the chosen cell the current cell and mark it as visited
+            2.2 Else if stack is not empty
+            2.2.1 Pop a cell from the stack
+            2.2.2 Make it the current cell */
+
+        public class Maker : Maze
+        {
+            public bool IsDone { get; private set; }
+            public Cell CurrentCell { get; private set; }
+            public int StepsCount { get; private set; }
+            private Stack<Cell> cellsStack = new Stack<Cell>();
+            private Random random = new Random();
+
+            public Maker(int columns, int rows) : base(columns, rows)
+            {
+                IsDone = false;
+                StepsCount = 0;
+                CurrentCell = Cells[CellCoordinate(1, 1)];
+            }
+
+            public void Make()
+            {
+                while (!IsDone) //Step 2
+                    MakeStep();
+            }
+
+            public void MakeStep()
+            {
+                CurrentCell.IsVisited = true; //Step 2.1.4
+                List<Cell> neighboringCells = GetUnvisitedNeighboringCellsList(CurrentCell);
+                if (neighboringCells.Count > 0) //Step 2.1
+                {
+                    Cell chosenCell = neighboringCells[random.Next(neighboringCells.Count)]; //Step 2.1.1
+                    cellsStack.Push(CurrentCell); //Step 2.1.2
+                    RemoveWallBetween2Cells(CurrentCell, chosenCell); //Step 2.1.3
+                    CurrentCell = chosenCell; //Step 2.1.4
+                }
+                else if (cellsStack.Count > 0) //Step 2.2
+                    CurrentCell = cellsStack.Pop(); //Step 2.2.1 and 2.2.2
+                else
+                    IsDone = true;
+                StepsCount++;
+            }
+
+            private void RemoveWallBetween2Cells(Cell currentCell, Cell chosenCell)
+            {
+                int index = 0;
+                if (currentCell.X - chosenCell.X == -2)
+                    index = CellCoordinate(currentCell.X + 1, currentCell.Y);
+                else if (currentCell.X - chosenCell.X == 2)
+                    index = CellCoordinate(currentCell.X - 1, currentCell.Y);
+                else if (currentCell.Y - chosenCell.Y == -2)
+                    index = CellCoordinate(currentCell.X, currentCell.Y + 1);
+                else if (currentCell.Y - chosenCell.Y == 2)
+                    index = CellCoordinate(currentCell.X, currentCell.Y - 1);
+                Cells[index].IsWall = false;
+            }
+        }
+    }
+}
